7ae0f51722a95c8a4211f2aff921b31286f14d54
Hoang Nhuoc Quy <hoangnhuocquy0@gmail.com>
2017-02-04 12:33:20 +0700

    ITO-000 add restTemplate client load balance.

--- src/main/java/onlinejudge/user/config/WebSecurityConfig.java
+++ src/main/java/onlinejudge/user/config/WebSecurityConfig.java
@@ -1,16 +1,26 @@
 package onlinejudge.user.config;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends ResourceServerConfigurerAdapter{
+	@Autowired
+	private Environment env;
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
@@ -22,9 +32,27 @@ public class WebSecurityConfig extends ResourceServerConfigurerAdapter{
 	public RemoteTokenServices tokenService() {
 	    RemoteTokenServices tokenService = new RemoteTokenServices();
 	    tokenService.setCheckTokenEndpointUrl(
-	      "http://localhost:8080/onlinejudge-ifa-oauth/oauth/check_token");
-	    tokenService.setClientId("userService");
-	    tokenService.setClientSecret("userService@password");
+	    		env.getProperty("onlinejudge.ms.user.checkToken"));
+	    tokenService.setClientId(env.getProperty("onlinejudge.ms.user.id"));
+	    tokenService.setClientSecret(env.getProperty("onlinejudge.ms.user.password"));
+	    tokenService.setRestTemplate(restTemplate());
 	    return tokenService;
 	}
+	
+	@LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.setErrorHandler(new DefaultResponseErrorHandler() {
+			@Override
+			// Ignore 400
+			public void handleError(ClientHttpResponse response) throws IOException {
+				if (response.getRawStatusCode() != 400) {
+					super.handleError(response);
+				}
+			}
+		});
+        return restTemplate;
+    }
+
 }
