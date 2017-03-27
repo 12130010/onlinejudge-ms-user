f49ddc444c66dd9c10264a3b438cc99e22546088
Hoang Nhuoc Quy <hoangnhuocquy0@gmail.com>
2017-03-27 23:31:43 +0700

    ITO-000 Create endpoint user-003

--- src/main/java/onlinejudge/user/controller/UserController.java
+++ src/main/java/onlinejudge/user/controller/UserController.java
@@ -5,6 +5,8 @@ import java.security.Principal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.MessageSourceAware;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,16 +14,22 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import onlinejudge.domain.User;
 import onlinejudge.dto.MyResponse;
+import onlinejudge.message.util.MessageSourceUtil;
 import onlinejudge.user.service.UserService;
 
 @Controller
-public class UserController {
-	 private final Logger logger = LoggerFactory.getLogger(getClass());
+public class UserController implements MessageSourceAware{
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Autowired
+	private MessageSource messageSource; 
+	 
 	@Autowired
 	UserService userService;
 	
@@ -44,6 +52,7 @@ public class UserController {
 		logger.debug("/users : Create user - User: " + user.getEmail());
 		try {
 			user = userService.createUser(user);
+			//TODO validate user information
 			logger.info("Create user success! - User: " + user.getEmail());
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -51,7 +60,12 @@ public class UserController {
 		}
 		return user;
 	}
-	
+	/**
+	 * #user-002
+	 * Get user id by email
+	 * @param email
+	 * @return
+	 */
 	@RequestMapping("/get-user-id-by-email")
 	public @ResponseBody ResponseEntity<String> getUserIDByEmail(@RequestParam String email){
 		ResponseEntity<String> response  = null;
@@ -62,5 +76,30 @@ public class UserController {
 			response = new ResponseEntity<String>(user.getId(), HttpStatus.OK);
 		}
 		return response;
+	}
+	
+	/**
+	 * #user-003
+	 * Get user info by email
+	 * @param email
+	 * @return
+	 */
+	@RequestMapping(value="/users", method=RequestMethod.GET)
+	public @ResponseBody ResponseEntity<?> getUserByEmail(@RequestParam(defaultValue = "") String email){
+		ResponseEntity<?> response = null;
+		User user = userService.getUserByEmail(email);
+		if(user == null){
+			MyResponse myResponse = MyResponse.builder().fail().
+					setObj(MessageSourceUtil.getMessage(messageSource, "user.email.not.exist", email)).build();
+			response = new ResponseEntity<MyResponse>(myResponse, HttpStatus.BAD_REQUEST);
+		}else{
+			user.setPassword("******");
+			response = new ResponseEntity<User>(user, HttpStatus.OK);
+		}
+		return response;
+	}
+	@Override
+	public void setMessageSource(MessageSource messageSource) {
+		this.messageSource = messageSource;
 	}
 }
