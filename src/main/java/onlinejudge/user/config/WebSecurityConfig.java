6ed434a090d22b19971327a1822cfc2931957ba8
Hoang Nhuoc Quy <hoangnhuocquy0@gmail.com>
2017-05-01 22:02:18 +0700

    ITO-000 update API create User

--- src/main/java/onlinejudge/user/config/WebSecurityConfig.java
+++ src/main/java/onlinejudge/user/config/WebSecurityConfig.java
@@ -1,20 +1,9 @@
 package onlinejudge.user.config;
 
-import java.io.IOException;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.env.Environment;
-import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
-import org.springframework.web.client.DefaultResponseErrorHandler;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableWebSecurity
